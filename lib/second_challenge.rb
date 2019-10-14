f2f2f0eec0eb6b01c75c6a7f7d59a81dd96ba3ff
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-14 13:47:10 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends = []
       }}
 
   
